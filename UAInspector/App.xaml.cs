d10af1d5aa167657f68ddf5f6d635ba3886baac4
Quim Alborch <quimalborch@gmail.com>
2025-11-01 21:15:47 +0100

    Remove update management logic from App.xaml.cs
    The `UpdateMyApp` method, responsible for checking, downloading,
    and applying updates, has been removed. Additionally, the
    commented-out invocation of this method (`Task.Run(() =>
    UpdateMyApp());`) has been deleted. This change simplifies
    `App.xaml.cs` by focusing solely on application initialization
    and execution, removing update-related responsibilities.

--- UAInspector/App.xaml.cs
+++ UAInspector/App.xaml.cs
@@ -21,23 +21,6 @@ namespace UAInspector
             App app = new();
             app.InitializeComponent();
             app.Run();
-            //Task.Run(() => UpdateMyApp());
-        }
-
-        private static async Task UpdateMyApp()
-        {
-            var mgr = new UpdateManager("https://the.place/you-host/updates");
-
-            // check for new version
-            var newVersion = await mgr.CheckForUpdatesAsync();
-            if (newVersion == null)
-                return; // no update available
-
-            // download new version
-            await mgr.DownloadUpdatesAsync(newVersion);
-
-            // install new version and restart app
-            mgr.ApplyUpdatesAndRestart(newVersion);
         }
     }
 }
