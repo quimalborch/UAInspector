36a211241fc1bf41899d43435f6f44e0c0925a11
Quim Alborch <quimalborch@gmail.com>
2025-11-01 19:51:41 +0100

    Add Velopack integration and update handling logic
    Integrated Velopack library into the application by adding
    a `using Velopack;` directive and introducing a new `Main`
    method in `App.xaml.cs` as the application entry point.
    The `Main` method initializes and runs the Velopack app.
    
    Added an `UpdateMyApp` method to handle application updates,
    including checking for updates, downloading, and applying
    them with a restart.
    
    Updated `UAInspector.csproj` to include the Velopack package
    (version 0.0.1298) and adjusted the build configuration for
    `App.xaml` to ensure proper processing. General project
    file adjustments were made to support these changes.

--- UAInspector/App.xaml.cs
+++ UAInspector/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Velopack;
 
 namespace UAInspector
 {
@@ -13,5 +14,30 @@ namespace UAInspector
     /// </summary>
     public partial class App : Application
     {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            VelopackApp.Build().Run();
+            App app = new();
+            app.InitializeComponent();
+            app.Run();
+            //Task.Run(() => UpdateMyApp());
+        }
+
+        private static async Task UpdateMyApp()
+        {
+            var mgr = new UpdateManager("https://the.place/you-host/updates");
+
+            // check for new version
+            var newVersion = await mgr.CheckForUpdatesAsync();
+            if (newVersion == null)
+                return; // no update available
+
+            // download new version
+            await mgr.DownloadUpdatesAsync(newVersion);
+
+            // install new version and restart app
+            mgr.ApplyUpdatesAndRestart(newVersion);
+        }
     }
 }
